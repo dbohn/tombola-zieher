34c629501bd05b556989e5e8c66b7449d0782187
David Bohn <davbohn@googlemail.com>
2015-11-21 16:21:11 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,66 @@
+var serialPort = require("serialport");
+
+var express = require("express");
+
+var app = express();
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var port = null;
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+	res.sendFile('index.html', {
+		root: __dirname
+	});
+});
+
+function setupSerialPort(path) {
+	if (port != null && port.isOpen()) {
+		port.close(function() {
+			setupSerialPort(path)
+		});
+		return;
+	}
+
+	console.log('Connecting to ', path);
+	port = new serialPort.SerialPort(path, {
+		baudrate: 9600,
+		parser: serialPort.parsers.readline("\r\n")
+	});
+
+	port.on("open", function() {
+		console.log('opened serial connection');
+
+		port.on('data', function(data) {
+			if (data == 'Button opened') {
+				io.emit('evt', {type: "button", action: "released"});
+			}
+			console.log('data received: ' + data);
+		});
+	});
+}
+
+io.on('connection', function(socket) {
+	console.log('a user connected');
+	serialPort.list(function(err, ports) {
+		socket.emit('ports', ports);
+	});
+
+	socket.on('port-changed', function(port) {
+		setupSerialPort(port);
+	});
+
+	socket.on('disconnect', function() {
+		console.log('user disconnected');
+	});
+});
+
+var server = http.listen(3000, function() {
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Example app listening at http://%s:%s', host, port);
+});
---
+++ public/frontend.js
@@ -0,0 +1,72 @@
+var socket = io();
+
+var vm = new Vue({
+	el: '#app',
+	data: {
+		number: 0,
+		numbers: [],
+		pickedNumbers: [],
+		minNumber: 80,
+		maxNumber: 120,
+		ports: [],
+		selectedPort: {}
+	},
+	ready: function() {
+		$('.number').flowtype({
+			maxFont: 900,
+			fontRatio: 10
+		});
+		this.resetList();
+	},
+	methods: {
+		portChanged: function() {
+			socket.emit('port-changed', this.selectedPort.comName);
+		},
+		updatePorts: function(ports) {
+			this.ports = ports;
+			this.selectedPort = this.ports[0];
+			this.portChanged();
+		},
+		startShuffle: function() {
+			if (this.numbers.length > 0) {
+				var rand = Math.random();
+				var num = Math.floor(rand * this.numbers.length);
+				this.number = this.numbers[num];
+				this.pickedNumbers.unshift(this.number);
+				this.numbers.splice(num, 1);
+				console.log(this.pickedNumbers);
+			} else {
+				this.number = 0;
+			}
+		},
+		resetList: function() {
+			this.numbers = [];
+			this.pickedNumbers = [];
+			this.number = 0;
+			for (var i = 0; i < (this.maxNumber - this.minNumber + 1); i++) {
+				this.numbers[i] = this.minNumber + i;
+			}
+			console.log(this.numbers);
+		}
+	}
+});
+
+vm.$watch('minNumber', function(newVal, oldVal) {
+	vm.resetList();
+});
+
+vm.$watch('maxNumber', function(newVal, oldVal) {
+	vm.resetList();
+});
+
+// New ports list received
+socket.on('ports', function(ports) {
+	vm.updatePorts(ports);
+});
+
+// Button event received
+socket.on('evt', function(evt) {
+	if (evt.type == "button" && evt.action == "released") {
+		vm.startShuffle();
+	}
+});
