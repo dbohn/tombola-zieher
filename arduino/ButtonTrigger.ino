0842884ee0c22498c62dd11551fae19574f679df
David Bohn <davbohn@googlemail.com>
2015-12-02 21:10:54 +0100

    Switched Button from Active High to Active Low

--- arduino/ButtonTrigger.ino
+++ arduino/ButtonTrigger.ino
@@ -1,6 +1,6 @@
 #define BUTTON 3
 
-uint8_t buttonState = 0;
+uint8_t buttonState = 1;
 uint8_t buttonPrevious = 0;
 
 long lastDebounceTime = 0;
@@ -9,11 +9,13 @@ long debounceDelay = 50;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(BUTTON, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   int reading = digitalRead(BUTTON);
+  //Serial.println(reading);
 
   if (reading != buttonPrevious) {
     lastDebounceTime = millis();
@@ -23,7 +25,7 @@ void loop() {
     if (reading != buttonState) {
       buttonState = reading;
 
-      if (buttonState == HIGH) {
+      if (buttonState == LOW) {
         Serial.println("Button closed");
       } else {
         Serial.println("Button opened");
